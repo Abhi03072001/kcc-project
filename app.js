7399bf1a80fad61d597ad17427e82628dba9c9ef
Abhinav <pabhinav689@gmail.com>
2022-06-10 00:31:05 +0530

    log in

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var studentRouter = require('./routes/student');
+var teacherRouter = require('./routes/teacher');
+var loginRouter = require('./routes/login');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/student', studentRouter);
+app.use('/teacher', teacherRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
